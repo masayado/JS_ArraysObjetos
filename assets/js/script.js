da01362b733b1afcb46a057678f6a389805704ea
masayado <masayado@uc.cl>
2020-08-20 15:58:26 -0400

    agregue segunda variable traumatologia y su respectivo array de objetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.write('<p>Estadisticas Centro Medico Ñuñoa</p>');
 
-var radiologia = [
+var Radiologia = [
     {hora:"11:00", especialista:"IGNACIO SCHULZ", paciente:"FRANCISCA ROJAS", rut:"9.878.782-1", prevision:"FONASA"},
     {hora:"11:30", especialista:"FEDERICO SUBERCASEAUX", paciente:"PAMELA ESTRADA", rut:"15.345.241-3", prevision:"ISAPRE"},
     {hora:"15:00", especialista:"FERNANDO WURTHZ", paciente:"ARMANDO LUNA", rut:"16.445.345-9", prevision:"ISAPRE"},
@@ -8,4 +8,12 @@ var radiologia = [
     {hora:"16:00", especialista:"PATRICIA SUAZO", paciente:"RAMON ULLOA", rut:"14.989.389-K", prevision:"FONASA"}
 ]
 
-var = 
+var Traumatologia = [
+    {hora:"8:00", especialista:"MARIA PAZ ALTUZARRA", paciente:"PAULA SANCHEZ", rut:"15.554.774-5", prevision:"FONASA"},
+    {hora:"10:00", especialista:"RAUL ARAYA", paciente:"ANGÉLICA NAVAS", rut:"15.444.147-9", prevision:"ISAPRE"},
+    {hora:"10:30", especialista:"MARIA ARRIAGADA", paciente:"ANA KLAPP", rut:"17.879.423-9", prevision:"ISAPRE"},
+    {hora:"11:00", especialista:"ALEJANDRO BADILLA", paciente:"FELIPE MARDONES", rut:"1.547.423-6", prevision:"ISAPRE"},
+    {hora:"11:30", especialista:"CECILIA BUDNIK", paciente:"DIEGO MARRE", rut:"16.554.741-K", prevision:"FONASA"},
+    {hora:"12:00", especialista:"ARTURO CAVAGNARO", paciente:"CECILIA MENDEZ", rut:"9.747.535-8", prevision:"ISAPRE"},
+    {hora:"12:30", especialista:"ANDRES KANACRI", paciente:"MARCIAL SUAZO", rut:"11.254.785-5", prevision:"ISAPRE"}
+]
