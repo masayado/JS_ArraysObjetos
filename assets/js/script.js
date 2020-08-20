6e97618efd68e7f88de9fb7f9a5d08eb173b3aad
masayado <masayado@uc.cl>
2020-08-20 15:44:47 -0400

    agregue variable radiologia y su correspondiente array de objetos

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.write('<p>Estadisticas Centro Medico Ñuñoa</p>');
+
+var radiologia = [
+    {hora:"11:00", especialista:"IGNACIO SCHULZ", paciente:"FRANCISCA ROJAS", rut:"9.878.782-1", prevision:"FONASA"},
+    {hora:"11:30", especialista:"FEDERICO SUBERCASEAUX", paciente:"PAMELA ESTRADA", rut:"15.345.241-3", prevision:"ISAPRE"},
+    {hora:"15:00", especialista:"FERNANDO WURTHZ", paciente:"ARMANDO LUNA", rut:"16.445.345-9", prevision:"ISAPRE"},
+    {hora:"15:30", especialista:"ANA MARIA GODOY", paciente:"MANUEL GODOY", rut:"17.666.419-0", prevision:"FONASA"},
+    {hora:"16:00", especialista:"PATRICIA SUAZO", paciente:"RAMON ULLOA", rut:"14.989.389-K", prevision:"FONASA"}
+]
+
+var = 
